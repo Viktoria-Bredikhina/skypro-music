ee5bf0d28c24c342002719b90f499a6278f41a51
Viktoria <v.bredikhina2000@mail.ru>
2023-12-09 22:29:45 +0300

    tracks

--- src/components/TrackListItem/Tracks.jsx
+++ src/components/TrackListItem/Tracks.jsx
@@ -1,53 +1,69 @@
-import { BrowserRouter, Link } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import arrTracks from "../../utils/dataTracks";
-import "./Tracks.css";
+import * as S from "./Tracks.style";
 
-export function Tracks() {
+export function Tracks({ isLoading }) {
   const trackItems = arrTracks.map((track) => (
-    <li key={track.id} className="playlist__item">
-      <div className="playlist__track track">
-        <div className="track__title">
-          <div className="track__title-image">
-            <svg className="track__title-svg" alt="music">
+    <S.playlistItem key={track.id}>
+      <S.playlistTrack>
+        <S.trackTitle>
+          <S.trackTitleImage>
+            <S.trackTitleSvg alt="music">
               <use xlinkHref="img/icon/sprite.svg#icon-note" />
-            </svg>
-          </div>
-          <div className="track__title-text">
+            </S.trackTitleSvg>
+          </S.trackTitleImage>
+
+          {isLoading ? (
+            <div className="track__title-text">
+              <BrowserRouter>
+                <S.trackTitleLink href="http://">
+                  {track.trackName}
+                  {track.remix ? (
+                    <S.trackTitleSpan>({track.remix})</S.trackTitleSpan>
+                  ) : (
+                    ""
+                  )}
+                </S.trackTitleLink>
+              </BrowserRouter>
+            </div>
+          ) : (
+            <S.Skeleton> </S.Skeleton>
+          )}
+        </S.trackTitle>
+
+        {isLoading ? (
+          <S.trackAuthor>
             <BrowserRouter>
-              <Link className="track__title-link" href="http://">
-                {track.trackName}
-                {track.remix ? (
-                  <span className="track__title-span">({track.remix})</span>
-                ) : (
-                  ""
-                )}
-              </Link>
+              <S.trackAuthorLink href="http://">
+                {track.trackAuthor}
+              </S.trackAuthorLink>
             </BrowserRouter>
-          </div>
-        </div>
-        <div className="track__author">
-          <BrowserRouter>
-            <Link className="track__author-link" href="http://">
-              {track.trackAuthor}
-            </Link>
-          </BrowserRouter>
-        </div>
-        <div className="track__album">
-          <BrowserRouter>
-            <Link className="track__album-link" href="http://">
-              {track.album}
-            </Link>
-          </BrowserRouter>
-        </div>
+          </S.trackAuthor>
+        ) : (
+          <S.Skeleton> </S.Skeleton>
+        )}
+
+        {isLoading ? (
+          <S.trackAlbum>
+            <BrowserRouter>
+              <S.trackAlbumLink href="http://">
+                {track.album}
+              </S.trackAlbumLink>
+            </BrowserRouter>
+          </S.trackAlbum>
+        ) : (
+          <S.skeletonAlbum> </S.skeletonAlbum>
+        )}
+
         <div className="track__time">
-          <svg className="track__time-svg" alt="time">
+          <S.trackTimeSvg alt="time">
             <use xlinkHref="img/icon/sprite.svg#icon-like" />
-          </svg>
-          <span className="track__time-text"> {track.trackTime}</span>
+          </S.trackTimeSvg>
+          <S.trackTimeText> {track.trackTime}</S.trackTimeText>
         </div>
-      </div>
-    </li>
+      </S.playlistTrack>
+    </S.playlistItem>
   ));
 
-  return <ul className="content__playlist playlist">{trackItems}</ul>;
+  return <S.contentPlaylist>{trackItems}</S.contentPlaylist>;
 }
