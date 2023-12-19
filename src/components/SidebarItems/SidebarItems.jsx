e8d53aeae20da5af6868e523473cde6639d45881
Viktoria <v.bredikhina2000@mail.ru>
2023-12-19 14:10:20 +0300

    not found

--- src/components/SidebarItems/SidebarItems.jsx
+++ src/components/SidebarItems/SidebarItems.jsx
@@ -1,18 +1,17 @@
-import { BrowserRouter, Link } from "react-router-dom";
-import "./SidebarItems.css";
+import * as S from "./SidebarItems.style";
+import { SkeletonSidebar } from "../TrackListItem/Tracks.style";
 
 export function SidebarItems(props) {
   return (
-    <div className="sidebar__item">
-      {props.item.loading ? (<BrowserRouter>
-        <Link className="sidebar__link" href={props.item.link}>
-          <img
-            className="sidebar__img"
-            src={props.item.img}
-            alt="day's playlist"
-          />
-        </Link>
-      </BrowserRouter>) : ( <div className="skeleton skeleton__sidebar"> </div>)}
-    </div>
+    <S.sidebarItem>
+      {props.item.loading ? (
+
+          <S.sidebarLink href={props.item.link}>
+            <S.sidebarImg src={props.item.img} alt="day's playlist" />
+          </S.sidebarLink>
+      ) : (
+        <SkeletonSidebar> </SkeletonSidebar>
+      )}
+    </S.sidebarItem>
   );
 }
