7abab988055ec49a3150698607f736ef27c91e00
Viktoria <v.bredikhina2000@mail.ru>
2023-12-20 23:07:27 +0300

    mistakes are solved

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { GlobalStyle } from "./index.styles";
-import App from "./App";
+import * as S from "./components/TracklistTitle/TrackListTitle.style";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <GlobalStyle />
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+export function TrackListTitle() {
+    return (
+        <S.contentTitle>
+          <S.col01>Трек</S.col01>
+          <S.col02>ИСПОЛНИТЕЛЬ</S.col02>
+          <S.col03>АЛЬБОМ</S.col03>
+          <S.col04>
+            <S.playlistTitleSvg alt="time">
+              <use xlinkHref="img/icon/sprite.svg#icon-watch" />
+            </S.playlistTitleSvg>
+          </S.col04>
+        </S.contentTitle>
+    )
+}
