ff9f5a8084868afaa4e8721fabe1d66d3f73c7bb
Viktoria <v.bredikhina2000@mail.ru>
2023-12-20 22:30:20 +0300

    sing forms + redirects

--- src/pages/favorites/favorites.jsx
+++ src/pages/favorites/favorites.jsx
@@ -1,17 +1,16 @@
-import * as S from "../main/main.styles";
+import * as S from "../main/main.style";
 import { AudioPlayer } from "../../components/AudioPlayer/AudioPlayer";
 
-export function Favorites () {
-    return (
-        <S.wrapper>
+export function Favorites() {
+  return (
+    <S.wrapper>
       <S.container>
         <S.main>
-        <h1>FavoritesPages</h1>
+          <h1>FavoritesPages</h1>
         </S.main>
         <AudioPlayer />
         <footer className="footer" />
       </S.container>
-    </S.wrapper> 
-       
-    )
+    </S.wrapper>
+  );
 }
