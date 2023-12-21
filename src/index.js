a2221a12e64b24a90f9dac81af64baf0836ab7bb
Viktoria <v.bredikhina2000@mail.ru>
2023-12-21 17:36:34 +0300

    index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import * as S from "./components/TracklistTitle/TrackListTitle.style";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { GlobalStyle } from "./index.styles";
+import App from "./App";
 
-export function TrackListTitle() {
-    return (
-        <S.contentTitle>
-          <S.col01>Трек</S.col01>
-          <S.col02>ИСПОЛНИТЕЛЬ</S.col02>
-          <S.col03>АЛЬБОМ</S.col03>
-          <S.col04>
-            <S.playlistTitleSvg alt="time">
-              <use xlinkHref="img/icon/sprite.svg#icon-watch" />
-            </S.playlistTitleSvg>
-          </S.col04>
-        </S.contentTitle>
-    )
-}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+root.render(
+  <React.StrictMode>
+    <GlobalStyle />
+    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
