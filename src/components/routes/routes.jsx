1947de25cc50483d73fd7aa2e3eb9091374e8f7f
Viktoria <v.bredikhina2000@mail.ru>
2024-01-01 14:25:23 +0300

    SignIn/SignUp

--- src/components/routes/routes.jsx
+++ src/components/routes/routes.jsx
@@ -3,10 +3,15 @@ import { Main } from "./pages/main/main";
 import { NotFound } from "./pages/notFound/notFound";
 import { Favorites } from "./pages/favorites/favorites";
 import { Category } from "./pages/category/category";
+import { SignIn } from "./pages/login/signIn";
+import { SignUp } from "./pages/login/signUp";
+
 
 export function AppRoutes() {
   return (
     <Routes>
+      <Route path="/signIn" element={<SignIn />} />
+      <Route path="/signUp" element={<SignUp />} />
       <Route path="/" element={<Main />} />
       <Route path="/favorites" element={<Favorites />} />
       <Route path="/category/:id" element={<Category />} />
--- src/pages/login/signIn.jsx
+++ src/pages/login/signIn.jsx
@@ -1,12 +1,12 @@
 import * as S from "./login.style";
 import { SignInForm } from "../../components/SignInForm/SignInForm";
 
-export function SignIn({ onAuthButtonClick }) {
+export function SignIn() {
   return (
     <S.Wrapper>
       <S.ContainerEnter>
         <S.ModalBlock>
-          <SignInForm onAuthButtonClick={onAuthButtonClick} />
+          <SignInForm />
         </S.ModalBlock>
       </S.ContainerEnter>
     </S.Wrapper>
