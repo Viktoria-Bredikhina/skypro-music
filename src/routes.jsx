c6a29f94865fae80d9aa13f593c508403c08083a
Viktoria <v.bredikhina2000@mail.ru>
2024-01-01 14:03:40 +0300

    notFound

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import { Main } from "./pages/main/main";
+import { NotFound } from "./pages/notFound/notFound";
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
